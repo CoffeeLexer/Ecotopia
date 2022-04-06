656cced5d8a44d5bdf431d98736e53d9784e3d2e
T4rt4ru5 <vitkuseduardas@gmail.com>
2022-04-06 11:03:05 +0300

    Create challenge

--- challenge/create.js
+++ challenge/create.js
@@ -9,4 +9,15 @@ utilities.app.post('/challenge/create', async (req, res) => {
     let user = response.result[0]
     sql = ` insert into challenge (recommended_people_amount, fk_account, max_people_amount, description) 
             value ('${req.body.recommendedPeople}', '${user.id}', '${req.body.maxPeople}', '${req.body.description}')`
+    response = await utilities.query(sql)
+    let challenge_id = response.result.insertId
+    for(let i in req.body.tools) {
+        sql = `insert into tool (content, fk_challenge) value ('${req.body.tools[i]}', '${challenge_id}')`
+        response = await utilities.query(sql)
+    }
+    for(let i in req.body.pollutionTags) {
+        sql = `insert into tag_list(fk_challenge, fk_tag) value ('${challenge_id}', '${req.body.pollutionTags[i]}')`
+        response = await utilities.query(sql)
+    }
+    res.json({flag: "success"})
 })
