dba8c7146db4bcfd071bfc3710ce958f85d74850
T4rt4ru5 <vitkuseduardas@gmail.com>
2022-04-12 20:05:06 +0300

    "UnKnOwN error fix version 0.01"

--- challenge/create.js
+++ challenge/create.js
@@ -1,7 +1,7 @@
 const utilities = require('../utilities')
 
 utilities.app.post('/challenge/create', async (req, res) => {
-    let test = utilities.field_test(req.body, ['cookie', 'description', 'pollutionTags', 'tools', 'difficulty', 'name'])
+    let test = utilities.field_test(req.body, ['cookie', 'description', 'pollutionTags', 'difficulty', 'name'])
     if(test.flag === "failure") return res.json(test)
     let account = await utilities.account_find(req.body.cookie)
     if(account.flag === "failure") return res.json({flag: 'failure', msg: ['account with current cookie not found']})
@@ -9,10 +9,6 @@ utilities.app.post('/challenge/create', async (req, res) => {
             value ('${account.id}', '${req.body.description}', '${req.body.difficulty}', '${req.body.name}')`
     let response = await utilities.query(sql)
     let challenge_id = response.result.insertId
-    for(let i in req.body.tools) {
-        sql = `insert into tool (content, fk_challenge) value ('${req.body.tools[i]}', '${challenge_id}')`
-        response = await utilities.query(sql)
-    }
     for(let i in req.body.pollutionTags) {
         sql = `insert into tag_list(fk_challenge, fk_tag) value ('${challenge_id}', '${req.body.pollutionTags[i]}')`
         response = await utilities.query(sql)
