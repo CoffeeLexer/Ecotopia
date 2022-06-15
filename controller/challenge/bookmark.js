707c38535b4bcc99fcc554e8197ee4c788cdce91
T4rt4ru5 <vitkuseduardas@gmail.com>
2022-06-15 14:41:32 +0300

    State update

--- controller/challenge/bookmark.js
+++ controller/challenge/bookmark.js
@@ -12,7 +12,7 @@ async function getAll(req, res, next) {
     if(req.body.page && req.body.limit) {
         if(req.body.page < 1) return res.status(401).send(`Page starts at 1`)
         if(req.body.limit < 1) return res.status(401).send(`Limit starts at 1`)
-        postfix = ` limit ${req.body.limit} offset ${req.body.page}`
+        postfix = ` limit ${req.body.limit} offset ${req.body.page * req.body.limit}`
     }
     else {
         return res.status(401).send(`Wrong format. Body must have page AND limit or nothing!`)
